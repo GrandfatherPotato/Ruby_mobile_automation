9241ccaa25a09200649b215ff76e425182bff9a0
ValtsDrozdovs <valts.drozdovs@testdevlab.com>
2021-10-28 23:55:14 +0300

    .gitignore

--- features/screens/wishlist_screen.rb
+++ features/screens/wishlist_screen.rb
@@ -4,6 +4,7 @@ class WishlistScreen
     @open_main_menu = Elements.new(:id, 'action_to_dashboard')
     @open_item_category = Elements.new(:xpath, :"//android.widget.TextView[contains(@text, 'T-shirts')]")
     @wishlist_button = Elements.new(:xpath, :"//android.widget.TextView[@resource-id='pl.com.fourf.ecommerce:id/product_category_small_item_wish_list']")
+    @wishlist_menu = Elements.new(:id, 'action_to_wish_list')
   end
 
   def open_main_menu
@@ -14,13 +15,19 @@ class WishlistScreen
     @open_item_category.click
   end
 
+  # Not the best way to do it, but works for now
+  # Could add a search trough whole page and add elements later
+  # Just scroll the page after every 2 added elements and add them to the list
   def add_items_to_wishlist
     wishlist_button_arr = @wishlist_button.get_multiple_elements
-    i = 2
-    while i < 5 do
+    i = 0
+    while i < 4
       wishlist_button_arr[i].click
       i += 1
     end
   end
 
+  def open_wishlist
+    @wishlist_menu.click
+  end
 end
