24d6c1f2133630b5467e49f911cd6528671e246f
ValtsDrozdovs <valts.drozdovs@testdevlab.com>
2021-11-02 23:15:09 +0200

    .gitignore

--- features/screens/shopping_screen.rb
+++ features/screens/shopping_screen.rb
@@ -8,7 +8,7 @@ class ShoppingScreen
     @check_cart_button = Elements.new(:id, 'product_add_to_cart_confirm_cart')
     #It is possible to use id's here, but if more items are added, the id's are gone but this code is still correct
     @added_cart_item_name = Elements.new(:xpath, :"//android.widget.TextView[@resource-id='pl.com.fourf.ecommerce:id/cart_product_item_title']")
-    @added_cart_item_price = Elements.new(:xpath, :"//android.widget.TextView[@resource-id='pl.com.fourf.ecommerce:id/cart_product_item_title']")
+    @added_cart_item_price = Elements.new(:xpath, :"//android.widget.TextView[@resource-id='pl.com.fourf.ecommerce:id/cart_product_item_current_price']")
     @shopping_cart_product_value = Elements.new(:xpath, :"//androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[3]/android.widget.TextView[2]")
   end
 
@@ -22,19 +22,19 @@ class ShoppingScreen
   end
 
   def add_item_to_cart
-    @added_item_name = Elements.new(:xpath, :"//android.widget.TextView[@resource-id='pl.com.fourf.ecommerce:id/product_header_item_name']")
+    @added_item_name = Elements.new(:xpath, :"//android.widget.TextView[@resource-id='pl.com.fourf.ecommerce:id/product_header_item_name']").get_text
     @add_to_cart.click
   end
 
   def verify_added_items
     @check_cart_button.click
     added_item_name = @added_item_name
-    raise "ELEMENT WAS NOT ADDED: #{added_item_name}" unless @added_cart_item_name == added_item_name
+    raise "ELEMENT WAS NOT ADDED: #{added_item_name}" unless @added_cart_item_name.get_text == added_item_name
   end
 
   def verify_added_item_price
-    unless @added_cart_item_price == @shopping_cart_product_value
-      raise "ELEMENT PRICE NOT THE SAME: #{@added_cart_item_price}"
+    unless @added_cart_item_price.get_text == @shopping_cart_product_value.get_text
+      raise "ELEMENT PRICE NOT THE SAME: #{@added_cart_item_price.get_text}"
     end
   end
 end
